9328f9a55fa0b7fc2f704a58f9b94b87ecf4268b
JMunoz0402 <jesusmunoz0402@gmail.com>
2024-10-02 18:22:48 -0700

    assignment1

---
+++ assignment1.js
@@ -0,0 +1,27 @@
+// 1. Declare Variables
+// Using let
+let hero = "Max";
+let activity = "rode";
+let vehicle = "a flying pizza";
+let destination = "the moon";
+let timeOfDay = "a starry night";
+// Using const
+const event = "a cosmic disco";
+
+// 2. Create the Story
+console.log("--- Funny Space Adventure ---");
+console.log(hero + " was on " + destination + " during " + timeOfDay + ".");
+console.log("Out of nowhere, " + hero + " jumped on " + vehicle + " and " + activity + " off into the stars.");
+console.log("It turned out to be " + event + ", and aliens from all over the galaxy joined the fun!");
+
+// 3. Experiment with Different Values
+console.log("--- Different Version of the Space Adventure ---");
+// Change values to create a different story
+hero = "Luna";
+activity = "danced on";
+vehicle = "a rainbow cloud";
+destination = "Mars";
+timeOfDay = "a sunny morning";
+console.log(hero + " was on " + destination + " during " + timeOfDay + ".");
+console.log("Suddenly, " + hero + " spotted " + vehicle + " and " + activity + " like a pro.");
+console.log("It turned out to be " + event + ", and everyone partied in zero gravity!");
